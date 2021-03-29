0158edc25ca2042f0bb1ad8740fe1ba5ddf1751d
rpc08002 <rcurtin0491@gmail.com>
2021-03-29 18:51:13 -0400

    imported dependencies into tablearea component

---
+++ src/components/TableArea/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import PageHeader from '../PageHeader';
+import SearchBar from '../SearchBar';
+import TableData from '../TableData';
+import TableHeader from '../TableHeader';
+import getEmployeeName from "../../util/API";
