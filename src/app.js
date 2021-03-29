3ac5fa1058b7e55f71239d164790b513324c56db
rpc08002 <rcurtin0491@gmail.com>
2021-03-29 19:16:29 -0400

    components working and displaying info correctly

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TableArea from './components/TableArea';
 
 function App() {
   return (
--- src/components/TableArea/index.js
+++ src/components/TableArea/index.js
@@ -1,6 +1,92 @@
-import React from 'react';
-import PageHeader from '../PageHeader';
-import SearchBar from '../SearchBar';
-import TableData from '../TableData';
-import TableHeader from '../TableHeader';
+import React from "react";
+import PageHeader from "../PageHeader";
+import SearchBar from "../SearchBar";
+import TableData from "../TableData";
+import TableHeader from "../TableHeader";
 import getEmployeeName from "../../util/API";
+
+export default class TableArea extends React.Component {
+  state = {
+    search: "name",
+    employees: [],
+  };
+
+  componentDidMount = () => {
+    this.loadEmployees();
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  loadEmployees = () => [
+    getEmployeeName()
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          // API returns "results"
+          employees: response.data.results,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      }),
+  ];
+
+  searchEmployee = () => [
+    getEmployeeName()
+      .then((response) => {
+        console.log(response);
+
+        let filter = this.state.search;
+
+        let filteredList = response.data.results.filter((item) => {
+
+          let values = Object.values(item.name.first).join("").toLowerCase();
+          return values.indexOf(filter.toLowerCase()) !== -1;
+        });
+
+        this.setState({
+          employees: filteredList,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      }),
+  ];
+
+  handleInputSubmit = (event) => {
+    event.preventDefault();
+    console.log("stuff");
+
+    this.searchEmployee();
+  };
+
+  SortByName = (e) => {
+    function handleClick(e) {
+      e.preventDefault();
+      console.log("The link was clicked!");
+    }
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <PageHeader />
+
+        <SearchBar
+          search={this.state.search}
+          handleInputChange={this.handleInputChange}
+          handleSubmit={this.handleInputSubmit}
+        />
+
+        {/* <TableHeader /> */}
+        <TableHeader SortByName={this.SortByName} />
+        <TableData employees={this.state.employees} />
+      </div>
+    );
+  }
+}
