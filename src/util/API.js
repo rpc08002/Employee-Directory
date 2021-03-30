b57a510304e0c4ccc8ae277e5e656f4d6f048977
rpc08002 <rcurtin0491@gmail.com>
2021-03-29 20:56:47 -0400

    Updates

--- src/util/API.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default function getEmployeeName(query) {
-    const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
-    return axios.get(BASEURL);
-}
